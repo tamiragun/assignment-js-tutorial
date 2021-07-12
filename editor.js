3747b9bbe1d69694fb0ea07b481db478f2a6eecc
Tamira <t.gunzburg@gmail.com>
2021-07-12 11:04:25 +0200

    Initial commit

---
+++ editor.js
@@ -0,0 +1,23 @@
+//Select editor
+var editor = ace.edit("editor");
+
+//Set theme
+editor.setTheme("ace/theme/kuroir");
+
+//Set programming language
+editor.session.setMode("ace/mode/javascript");
+
+//Run code
+//Thanks to: https://www.youtube.com/watch?v=foQ8MjEvsJQ
+const executeCodeBtn = document.querySelector(".editor__run");
+executeCodeBtn.addEventListener("click", () => {
+  //Get input from the code editor
+  const userCode = editor.getValue();
+
+  //Run the user code
+  try {
+    new Function(userCode)();
+  } catch (e) {
+    console.error(e);
+  }
+});
