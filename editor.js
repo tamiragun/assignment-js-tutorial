1894030d11fd072aab44feaa4b0506f541a7633a
Tamira <t.gunzburg@gmail.com>
2021-07-12 16:33:17 +0200

    Propagate editors

--- editor.js
+++ editor.js
@@ -1,31 +1,47 @@
-//Select editor
-var editor = ace.edit("editor");
+function createEditors(input) {
+  //Select editor
+  var editor = ace.edit("e" + input);
 
-//Set theme
-editor.setTheme("ace/theme/kuroir");
+  //Set theme
+  editor.setTheme("ace/theme/kuroir");
 
-//Set programming language
-editor.session.setMode("ace/mode/javascript");
+  //Set programming language
+  editor.session.setMode("ace/mode/javascript");
 
-//Run code
-//Thanks to: https://www.youtube.com/watch?v=foQ8MjEvsJQ
-const executeCodeBtn = document.querySelector(".editor__run");
-executeCodeBtn.addEventListener("click", () => {
-  //Get input from the code editor
-  const userCode = editor.getValue();
+  //Run code
+  //Thanks to: https://www.youtube.com/watch?v=foQ8MjEvsJQ
+  const executeCodeBtn = document.querySelector("#br" + input);
+  executeCodeBtn.addEventListener("click", () => {
+    //Get input from the code editor
+    const userCode = editor.getValue();
 
-  //Run the user code
+    //Run the user code
+    try {
+      new Function(userCode)();
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  //Clear code
+  //Thanks to: https://www.youtube.com/watch?v=foQ8MjEvsJQ
+  const resetCodeBtn = document.querySelector("#bc" + input);
+  resetCodeBtn.addEventListener("click", () => {
+    //Clear the editor
+    editor.setValue("");
+  });
+}
+
+//createEditors("editor");
+
+let numberOfEditors = 12;
+let i = 1;
+
+while (i <= numberOfEditors) {
   try {
-    new Function(userCode)();
+    createEditors(i.toString());
   } catch (e) {
-    console.error(e);
+    //keep looping to the next editor;
   }
-});
-
-//Clear code
-//Thanks to: https://www.youtube.com/watch?v=foQ8MjEvsJQ
-const resetCodeBtn = document.querySelector(".editor__reset");
-resetCodeBtn.addEventListener("click", () => {
-  //Clear the editor
-  editor.setValue("");
-});
+  i++;
+}
